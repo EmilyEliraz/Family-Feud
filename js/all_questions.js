407fa8b391793c3e60f3ae25b0af2392d3b8b288
EmilyEliraz <102620298+EmilyEliraz@users.noreply.github.com>
2023-02-22 22:41:12 -0500

    Update all_questions.js
    Made this for my bestie's club, shoutout isa

--- js/all_questions.js
+++ js/all_questions.js
@@ -1,15 +1,41 @@
 // This is the file that contains all the question, answer and point value data.
 
 var json = [{
-	// Example Object
-	"question": "Question 1 goes here",
-	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+	"question": "What are the most popular space missions?",
+	"answers": ["Apollo 11", "Voyager 1 & 2","Hubble Launch", "Kepler", "James Webb Launch", "Curiosity Rover", "Opportunity Rover", "New Horizons"],
+	"points": [20, 19, 17, 15, 12, 8, 5, 4]
 },{
-	// Example Object
-	"question": "Question 2 goes here",
-	"answers": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-	"points": [21, 17, 16, 16, 8, 8, 7, 7]
+	"question": "What are the size-types of stars?",
+	"answers": ["G (The sun)", "M", "A", "B", "O", "K", "F"],
+	"points": [27, 23, 15, 12, 10, 7, 6]
+},{
+	"question": "We asked 100 people, what is your favourite planet?",
+	"answers": ["Earth", "Saturn", "Mars", "Neptune", "Jupiter", "Uranus"],
+	"points": [36, 23, 15, 12, 8, 6]
+},{
+	"question": "We asked 100 astrophysicists, what’s the first thing that comes to mind when you think of space?",
+	"answers": ["Stars", "Asteroids/Meteors", "Galaxies", "The Universe", "Black Holes", "Planets"],
+	"points": [47, 26, 13, 6, 5, 3]	
+},{
+	"question": "We asked 100 people, who’s the most known astronomy person?",
+	"answers": ["Isabella Rivera", "Neil Degrasse Tyson", "Neil Armstrong", "Chris Hadfield", "Stephen Hawking"],
+	"points": [50, 30, 10, 5, 5]
+},{
+	"question": "What is the most famous constellation?",
+	"answers": ["Ursa Major", "Ursa Minor", "Orion", "Cassiopeia", "Leo", "Saggitarius", "Canus Major"],
+	"points": [26, 24, 22, 17, 12, 6, 4]	
+},{
+	"question": "We asked 100 astronomers: what is your zodiac sign?",
+	"answers": ["Astrology is not real", "Aries", "Pisces", "Scorpio", "Cancer", "Taurus", "Libra", "Saggitarius"],
+	"points": [91, 1, 1, 1, 1, 1, 1, 1]	
+},{
+	"question": "We asked 100 astronomy students: what was their favorite subject in astronomy?",
+	"answers": ["Cosmology", "Astrophysics", "Planetary Science", "Radio Astronomy", "Anything about Aliens"],
+	"points": [38, 24, 19, 17, 2]
+},{
+	"question": "We asked 100 Bloor students: what is their favorite club at Bloor?",
+	"answers": ["Astronomy Club", "Art Club", "Eco Club", "Girls in STEM", "Off the Streets",3 "Arctos Robotics"],
+	"points": [75, 10, 6, 5, 3, 1]	
 }
-	//...Append additional questions here
+	
 ];
